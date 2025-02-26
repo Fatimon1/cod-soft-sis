2269941e8a0a10044b5ca003aa1cddd4c2e57737
Fatimon1 <gonzalez.fatima.2024@cbtis-246.edu.mx>
2025-02-26 17:13:23 -0600

    Add files via upload

---
+++ practica 4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+uno=input("ingresa tu nombre")
+dos=input("ingresa tu edad")
+tres=input("ingresa que quieres ser")
+print("mi nombre es",uno,"tengo",dos,"años","y quiero ser",tres)
+        
+
